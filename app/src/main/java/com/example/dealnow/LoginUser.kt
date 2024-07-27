9af05e5e3a3c6c6776a52d9ca7aee0ee585a7a25
Noorkbr <freelancer.noorkbr@gmail.com>
2024-07-27 14:38:53 +0600

    Login

---
+++ app/src/main/java/com/example/dealnow/LoginUser.kt
@@ -0,0 +1,10 @@
+package com.example.dealnow
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+
+data class LoginUser(
+
+    val phoneNumber: String,
+    val password : String
+
+)
--- app/src/main/java/com/example/dealnow/loginFragment.kt
+++ app/src/main/java/com/example/dealnow/loginFragment.kt
@@ -5,25 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isEmpty
 import androidx.navigation.fragment.findNavController
 import com.example.dealnow.databinding.FragmentLoginBinding
 import com.example.fitglow.base.BaseFragment
 
 
-
 class loginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
-
-
 
     override fun setListener() {
 
-        binding.loginbtn.setOnClickListener {
+        with(binding) {
+            loginbtn.setOnClickListener {
+                phoneNumberEdittext.isEmpty()
+                passwordEdittext.isEmpty()
 
-                findNavController().navigate(R.id.action_loginFragment_to_startedFragment)
+                if (!phoneNumberEdittext.isEmpty() && !passwordEdittext.isEmpty()) {
+                    val user = LoginUser(
+                        phoneNumberEdittext.text.toString(),
+                        passwordEdittext.text.toString()
+                    )
+
+                    userLoginValidation(user)
+                }
 
             }
 
+        }
 
 
         binding.signupbtn.setOnClickListener {
@@ -37,10 +46,27 @@ class loginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
         }
 
 
-
     }
 
     override fun allObserver() {
 
     }
+
+    private fun userLoginValidation(user: LoginUser) {
+
+        if (user.phoneNumber.length == 11) {
+            if (user.password.length >= 8) {
+                findNavController().navigate(R.id.action_loginFragment_to_startedFragment)
+                /*  viewModel.login(loginUser)*/
+            } else {
+                Toast.makeText(context, "enter correct password", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(context, "enter correct phone/password", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
+
+
+
